8abe10521a1c99363671b6172c1fa360948e85c4
Ian Petersen <ispeters@gmail.com>
2026-07-17 09:09:07 -0700

    Add a Nix flake (#6)
    This diff introduces `flake.nix` and `flake.lock`, which allows me to
    run
    ```sh
    $ nix develop
    ```
    from the repo root and get a development environment with Clang 22,
    CMake 4.3.4, Ninja, Gersemi, etc. Inside that devenv,
    `cmake --workflow llvm-debug` Just Works™. The flake only supports
    nix-darwin on Apple silicon at the moment, but I intend to add Linux
    support One Day™.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+  # nixpkgs-unstable has CMake 4.1.something, but our CMakeLists.txt
+  # (optionally) uses features of CMake 4.3, so pull in the staging-next
+  # channel, which already has a CMake 4.3.4 package.
+  inputs.nixpkgs-staging.url = "github:NixOS/nixpkgs/staging-next";
+  outputs = { self, nixpkgs, nixpkgs-staging }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+      # I adapted this line from some Claude Sonnet 5 output; I don't
+      # understand why this syntax is so different from the line above
+      pkgs-staging = import nixpkgs-staging { inherit system; };
+    in
+    {
+      devShells.${system} = {
+        # default development shell with Clang 22
+        # launch with `nix develop`
+        default = pkgs.mkShell.override {
+          stdenv = pkgs.llvmPackages_22.libcxxStdenv;
+        } {
+          packages = with pkgs; [
+            clang-tools
+            pkgs-staging.cmake
+            gersemi
+            ninja
+          ];
+        };
+      };
+    };
+}
